1580f0c7d53f92d093bf563574f096b50ca6130e
Michel Oliveira <michel.oliveira999@hotmail.com>
2023-06-05 21:08:00 -0300

    feat: Using try catch

--- api/src/app/useCases/categories/createCategory.ts
+++ api/src/app/useCases/categories/createCategory.ts
@@ -2,7 +2,12 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function createCategory(request: Request, response: Response) {
-  const { icon, name } = request.body;
-  const category = await Category.create({ icon, name });
-  response.json(category);
+  try {
+    const { icon, name } = request.body;
+    const category = await Category.create({ icon, name });
+    response.status(201).json(category);
+  } catch (error) {
+    console.log(error);
+    response.status(500);
+  }
 }
