60d721b432c201ec1f732e7f0f46635adb3eed3c
Michel Oliveira <michel.oliveira999@hotmail.com>
2023-06-14 21:01:27 -0300

    feat: Creating header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { GlobalStyles } from './styles/GlobalStyles';
+import { Header } from './components/Header';
 
 export function App() {
   return (
     <>
       <GlobalStyles />
-      <h1>Hello world</h1>
+      <Header />
     </>
   );
 }
