87f775972602598397d755aa53a86efdefc50db0
Michel Oliveira <michel.oliveira999@hotmail.com>
2023-07-17 22:59:54 -0300

    feat: Creating the TableModal component

--- mobile/src/Main/index.tsx
+++ mobile/src/Main/index.tsx
@@ -10,8 +10,17 @@ import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
 import { Button } from '../components/Button';
+import { TableModal } from '../components/TableModal';
+import { useState } from 'react';
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('');
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -25,11 +34,18 @@ export function Main() {
       </Container>
       <Footer>
         <FooterContainer>
-          <Button onPress={() => alert('Novo pedido')}>
-            Novo pedido
-          </Button>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo pedido
+            </Button>
+          )}
         </FooterContainer>
       </Footer>
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   )
 }
