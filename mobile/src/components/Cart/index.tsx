44c0ecb71024f821d3bef24314815883d613bd11
Michel Oliveira <michel.oliveira999@hotmail.com>
2023-07-19 23:03:15 -0300

    feat: Creating the Cart component

---
+++ mobile/src/components/Cart/index.tsx
@@ -0,0 +1,61 @@
+import { FlatList, TouchableOpacity } from "react-native";
+import { CartItem } from "../../types/CartItem";
+
+import {
+  Item,
+  ProductContainer,
+  Actions,
+  Image,
+  QuantityContainer,
+  ProductDetails,
+} from './styles';
+import { Text } from "../Text";
+import { formatCurrency } from "../../utils/formatCurrency";
+import { PlusCircle } from "../Icons/PlusCircle";
+import { MinusCircle } from "../Icons/MinusCircle";
+
+interface CartProps {
+  cartItems: CartItem[]
+}
+
+export function Cart({ cartItems }: CartProps) {
+  return (
+    <FlatList
+      data={cartItems}
+      keyExtractor={cartItem => cartItem.product._id}
+      showsVerticalScrollIndicator={false}
+      renderItem={({ item: cartItem }) => (
+        <Item>
+          <ProductContainer>
+            <Image
+              source={{
+                uri: `http://172.18.0.1:3001/uploads/${cartItem.product.imagePath}`,
+              }}
+            />
+            <QuantityContainer>
+              <Text size={14} color="#666">
+                {cartItem.quantity}x
+              </Text>
+            </QuantityContainer>
+            <ProductDetails>
+              <Text size={14} weight="Semibold">
+                {cartItem.product.name}
+              </Text>
+              <Text size={14} color="#666" style={{ marginTop: 4 }}>
+                {formatCurrency(cartItem.product.price)}
+              </Text>
+            </ProductDetails>
+          </ProductContainer>
+          <Actions>
+            <TouchableOpacity style={{ marginRight: 24 }}>
+              <PlusCircle />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <MinusCircle />
+            </TouchableOpacity>
+          </Actions>
+        </Item>
+      )}
+    />
+  )
+};
