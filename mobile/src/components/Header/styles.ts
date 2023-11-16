027ba6ab77b7f150b877c4de389cff31deaeb471
Michel Oliveira <michel.oliveira999@hotmail.com>
2023-11-15 23:29:56 -0300

    feat: Creating the notification button

--- mobile/src/components/Header/styles.ts
+++ mobile/src/components/Header/styles.ts
@@ -1,7 +1,23 @@
 import { styled } from "styled-components/native";
+import { Platform } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 export const Container = styled.View`
   margin: 24px 24px 0;
+`;
+
+export const HeaderContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const NotificationContainer = styled.View`
+  background-color: #FFFFFF;
+  border-radius: 50px;
+  padding: 10px;
+  box-shadow: 0px 4px 2px rgba(0, 0, 0, ${isAndroid ? 1 : 0.1});
 `;
 
 export const Content = styled.View`
