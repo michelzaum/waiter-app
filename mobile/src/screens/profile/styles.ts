713bb758af066107ef7b53f5e175fe250035f08e
Michel Oliveira <michel.oliveira999@hotmail.com>
2023-12-12 22:44:44 -0300

    feat: Creating the profile screen

---
+++ mobile/src/screens/profile/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components/native";
+
+export const ProfileContainer = styled.View`
+  background-color: #FAFAFA;
+  flex: 1;
+`;
+
+export const ProfileHeader = styled.View`
+  padding: 48px 24px;
+`;
+
+export const ProfileFormContainer = styled.View`
+  padding: 0 24px;
+  gap: 24px;
+  margin-bottom: 40px;
+`;
+
+export const FormField = styled.View`
+  gap: 8px;
+`;
+
+export const Input = styled.TextInput`
+  color: #666666;
+  border: 1px solid #CCCCCC;
+  border-radius: 8px;
+  padding: 16px;
+`;
+
+export const SubmitContainer = styled.View`
+  padding: 0 24px;
+`;
