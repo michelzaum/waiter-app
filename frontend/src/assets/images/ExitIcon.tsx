64016b91359c43ccd46bf417c50aafc97f1ffe68
Michel Oliveira <michel.oliveira999@hotmail.com>
2024-04-18 22:01:25 -0300

    refactor: Changing svgs icons to functional components

---
+++ frontend/src/assets/images/ExitIcon.tsx
@@ -0,0 +1,19 @@
+export function ExitIcon() {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.657 3.34302C17.781 6.46702 17.781 11.533 14.657 14.657C11.533 17.781 6.467 17.781 3.343 14.657C0.219 11.533 0.219 6.46702 3.343 3.34302M9 1V9"
+        stroke="#666666"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
